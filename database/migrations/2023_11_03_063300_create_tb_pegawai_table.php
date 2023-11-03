e281ce28e15cf8b631eddcae13347a7e5840a05d
tikawahyuningtyas8@gmail.com <66901798+tikawahyu@users.noreply.github.com>
2023-11-03 16:42:48 +0700

    update tabel

--- database/migrations/2023_11_03_063300_create_tb_pegawai_table.php
+++ database/migrations/2023_11_03_063300_create_tb_pegawai_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tb_pegawai', function (Blueprint $table) {
-            $table->string('kode_pegawai',100);
+            $table->string('kode_pegawai',100)->primary();
             $table->integer('nik');
             $table->integer('nuptk');
             $table->string('nama',250);
