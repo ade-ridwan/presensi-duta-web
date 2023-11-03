dc784a2deceb802ebe1e56d65cc99c20ce09796b
Ade Ridwan <sukun024@gmail.com>
2023-11-03 16:14:54 +0700

    menambhakan dashbaord admin

---
+++ resources/views/admin/pages/dashboard/index.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.layouts.master')
+@section('content')
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <div class="card">
+            <div class="card-body">
+                <h3>Dashboard</h3>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,17 @@ Route::get('/', function () {
 });
 
 Route::get('/login', function () {
-    return view('admin.pages.login');
+    return view('admin.pages.auth.login');
 });
+
+Route::get('/register', function () {
+    return view('admin.pages.auth.register');
+});
+
+Route::prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::get('/dashboard', function () {
+            return view('admin.pages.dashboard.index');
+        })->name('dashboard');
+    });
