c4e3691c9bada47f1f512ee3868604548047ced7
tikawahyuningtyas8@gmail.com <66901798+tikawahyu@users.noreply.github.com>
2023-12-06 16:39:13 +0700

    menambah foto profil

--- resources/views/pegawai/layouts/navbar.blade.php
+++ resources/views/pegawai/layouts/navbar.blade.php
@@ -29,7 +29,7 @@
             <li class="nav-item navbar-dropdown dropdown-user dropdown">
                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <div class="avatar avatar-online">
-                        <img src="{{ asset('template/assets/img/avatars/1.png') }}" alt
+                        <img src="{{ asset('storage/' . auth()->user()->pegawai->foto)}}" alt
                             class="w-px-40 h-auto rounded-circle" />
                     </div>
                 </a>
@@ -39,7 +39,7 @@
                             <div class="d-flex">
                                 <div class="flex-shrink-0 me-3">
                                     <div class="avatar avatar-online">
-                                        <img src="{{ asset('template/assets/img/avatars/1.png') }}" alt
+                                        <img src="{{ asset('storage/' . auth()->user()->pegawai->foto )}}" alt
                                             class="w-px-40 h-auto rounded-circle" />
                                     </div>
                                 </div>
