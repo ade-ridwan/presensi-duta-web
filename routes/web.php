be31a2fdd071bc4fcf7978c3f4bbe1339feea40b
Ade Ridwan <sukun024@gmail.com>
2023-12-21 15:15:49 +0700

    change routes

--- routes/web.php
+++ routes/web.php
@@ -31,10 +31,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+Route::get('/', [AuthController::class, 'login'])->name('login');
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::class, 'authenticate'])->name('login.authenticate');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
